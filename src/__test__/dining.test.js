ee3fbf432b1547fdb7ca0186509f8358eaecec97
Austin Liu <austin.liu05@yahoo.com>
2024-02-11 12:51:40 -0500

    testing environment differences

--- src/__test__/dining.test.js
+++ src/__test__/dining.test.js
@@ -31,11 +31,13 @@ describe('Dining Hall Open Check', () => {
         expect(isVDubOpen(vdubHours)).toBe(true);
     });
     it('Testing Ivy Room Hours', () => {
-        console.log('Current time:', new Date().toISOString());
-console.log('Ivy hours:', ivyHours);
+        const mockedDate = new Date('2023-02-12T17:00:00Z');
+        jest.setSystemTime(mockedDate);
+        
+        console.log('Fake system time:', mockedDate.toISOString()); // Logs the set fake time        
 
         // closed Sunday at 12:00pm
-        jest.setSystemTime(new Date('2023-02-12T17:00:00Z')); 
+        // jest.setSystemTime(new Date('2023-02-12T17:00:00Z')); 
         expect(isIvyOpen(ivyHours)).toBe(false);
         // open Sunday at 7:00pm
         jest.setSystemTime(new Date('2023-02-12T22:00:00Z')); 
