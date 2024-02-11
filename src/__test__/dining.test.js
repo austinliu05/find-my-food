0bc2445ade93b15235ee25a306d55336f67a8947
Austin Liu <austin.liu05@yahoo.com>
2024-02-11 01:51:06 -0500

    Added Dining Status hook unit testing

---
+++ src/__test__/dining.test.js
@@ -0,0 +1,75 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useDiningStatus from '../hooks/useDiningStatus';
+import { isRattyOpen, isAndrewsOpen, isIvyOpen, isVDubOpen } from '../utils';
+import { rattyHours, andrewsHours, ivyHours, vdubHours } from '../constants'
+
+jest.mock('../constants', () => {
+    // Require the actual module
+    const actualConstants = jest.requireActual('../constants');
+    
+    return actualConstants
+  });
+
+jest.mock('../utils', () => {
+    // Require the actual module
+    const actualUtils = jest.requireActual('../utils');
+    
+    return actualUtils
+  });
+
+describe('Dining Hall Open Check', () => {
+    beforeAll(() => {
+        jest.useFakeTimers('modern');
+    });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
+    it('Basic Testing all Dining Hall Hours', () => {
+        jest.setSystemTime(new Date('2023-02-14T17:00:00Z')); // 12:00 PM
+        expect(isRattyOpen(rattyHours)).toBe(true);
+        expect(isAndrewsOpen(andrewsHours)).toBe(true);
+        expect(isIvyOpen(ivyHours)).toBe(true);
+        expect(isVDubOpen(vdubHours)).toBe(true);
+    });
+    it('Testing Ivy Room Hours', () => {
+        // closed Sunday at 12:00pm
+        jest.setSystemTime(new Date('2023-02-12T17:00:00Z')); // 12:00 PM
+        expect(isIvyOpen(ivyHours)).toBe(false);
+        // open Sunday at 7:00pm
+        jest.setSystemTime(new Date('2023-02-12T22:00:00Z')); // 7:00 PM
+        expect(isIvyOpen(ivyHours)).toBe(true);
+    });
+    it('Testing VDub Hours', () => {
+        // Closed on Sundays 
+        jest.setSystemTime(new Date('2023-02-12T17:00:00Z')); // 12:00 PM
+        expect(isVDubOpen(vdubHours)).toBe(false);
+        // Closed on Saturdays 
+        jest.setSystemTime(new Date('2023-02-11T17:00:00Z')); // 12:00 PM
+        expect(isVDubOpen(vdubHours)).toBe(false);
+    });
+    it('Basic Testing all Dining Hall Hours (closed)', () => {
+        jest.setSystemTime(new Date('2023-02-14T7:00:00Z')); // 2:00 AM
+        expect(isRattyOpen(rattyHours)).toBe(false);
+        expect(isAndrewsOpen(andrewsHours)).toBe(false);
+        expect(isIvyOpen(ivyHours)).toBe(false);
+        expect(isVDubOpen(vdubHours)).toBe(false);
+    });
+});
+
+describe('useDiningStatus', () => {
+    it('Testing utility functions', () => {
+        jest.useFakeTimers('modern');
+        jest.setSystemTime(new Date('2023-02-14T19:00:00Z')); // 4:00 PM
+
+        // Render the hook
+        const { result } = renderHook(() => useDiningStatus());
+
+        // Assert the initial state of the hook
+        expect(result.current.ratty).toBe(true);
+        expect(result.current.andrews).toBe(true);
+        expect(result.current.ivy).toBe(false);
+        expect(result.current.vdub).toBe(true);
+    });
+});
